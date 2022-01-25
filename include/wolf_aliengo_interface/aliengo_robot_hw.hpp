c4693f8f27bf4834d66ce1006299e1c5e547c383
Gennaro Raiola <gennaro.raiola@gmail.com>
2022-01-25 23:53:37 +0100

    Update aliengo hw interface to the new wolf hw interface

--- include/wolf_aliengo_interface/aliengo_robot_hw.hpp
+++ include/wolf_aliengo_interface/aliengo_robot_hw.hpp
@@ -10,14 +10,11 @@
 namespace aliengo2ros
 {
 
-
 class AliengoRobotHw : public hardware_interface::RobotHW, public hardware_interface::WolfRobotHwInterface
 {
 public:
   AliengoRobotHw();
   virtual ~AliengoRobotHw();
-
-  virtual bool registerInterfaces();
 
   void init();
   void read();
@@ -25,16 +22,16 @@ public:
 
 private:
 
-  // Aliengo's parameters
-  std::array<int, 12> aliengo_motor_idxs_ {{
-      aliengohal::FL_0, aliengohal::FL_1, aliengohal::FL_2, // LF
+  /** Map Aliengo internal joint indices to WoLF joints order */
+  std::array<unsigned int, 12> aliengo_motor_idxs_
+          {{
+          aliengohal::FL_0, aliengohal::FL_1, aliengohal::FL_2, // LF
           aliengohal::RL_0, aliengohal::RL_1, aliengohal::RL_2, // LH
           aliengohal::FR_0, aliengohal::FR_1, aliengohal::FR_2, // RF
           aliengohal::RR_0, aliengohal::RR_1, aliengohal::RR_2, // RH
-                                           }};
+          }};
 
-  // Aliengo-HAL
-  // std::shared_ptr<aliengohal::LowLevelInterface> aliengo_interface_;
+  /** Aliengo-HAL */
   aliengohal::LowLevelInterface aliengo_interface_;
   aliengohal::LowState aliengo_state_ = {0};
   aliengohal::LowCmd aliengo_lowcmd_ = {0};
@@ -44,12 +41,6 @@ private:
 
   /** @brief Executes the robot's startup routine */
   void startup_routine();
-
-  Eigen::Matrix3d world_R_base_;
-  Eigen::Vector3d world_ang_vel_base_;
-  Eigen::Vector3d world_lin_acc_base_;
-  Eigen::Vector3d vector3d_tmp_;
-  Eigen::Quaterniond quaterniond_tmp_;
 
 };
 
